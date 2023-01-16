03033d0769c82054738afa509913f94444fb8404
tanishajn12 <119069305+tanishajn12@users.noreply.github.com>
2023-01-16 19:24:00 +0530

    Create Assignment1_Q2.cpp

---
+++ Assignment1_Q2.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin>>n;
+
+    if(n>90) {
+        cout<<"EXCELLENT";
+    
+    }
+
+    else if(n>80 && n<=90) {
+        cout<<"GOOD";
+    }
+
+    else if(n>70 && n<=80) {
+        cout<<"FAIR";
+    }
+
+    else if(n>60 && n<=70) {
+        cout<<"MEET EXPECTATIONS";
+    }
+
+    else{
+        cout<<"BELOW AVERAGE";
+    }
+
+    return 0;
+}
